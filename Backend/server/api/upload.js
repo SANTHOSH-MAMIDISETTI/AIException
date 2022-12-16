cdc4c02e78afcbfb469829300bcd86cee47469e1
Santhosh Mamidisetti <mamidisettisanthosh2004@gmail.com>
2022-12-17 00:15:01 +0530

    almost the final thing

--- Backend/server/api/upload.js
+++
@@ -1,8 +0,0 @@
-const S3 = require('./s3');
-
-module.exports = async (req, res) => {
-    const {extension} = req.params;
-    const url = await S3.generateUploadURL(extension);
-    res.send({ url });
-  
-};
--- README.md
+++
@@ -1,2 +0,0 @@
-# AIException
-lablab.ai hackthon's implementation by team AIException.
